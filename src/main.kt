12c7503a8240baf9e79971f5a8a008026ff51740
Grace Waceke <gracewacekemwangi@gmail.com>
2022-03-10 13:10:15 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,33 @@
+fun main(){
+    var name= "codeHive"
+    var char =name[4]
+    var char1 = name[5]
+    var char2 = name[6]
+    var char3 = name[7]
+    var value =("$char$char1$char2$char3")
+    println(value)
+}
+fun remainder(){
+
+    var remainder= (5)
+    println(remainder)
+    var girl = ("Hi,my name is Grace and I am 24 years old.")
+    var line = ("Kenya is lovely")
+}
+fun theremainder(no1:Int, no2:Int):Int{
+    var modulus = no1 % no2
+    return modulus
+}
+var girl (name.contentToString){
+    var name=("Hi,my name is p and I am z years old.")
+    println (name)
+}
+fun line(name.String):String {
+    println(name.length)
+    return line
+
+}
+
+
+
+}
